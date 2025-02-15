4deafabc4f8a64b2612916647ffc8eb4e69a3d81
Klee Collage <klee.collage@gmail.com>
2025-02-14 21:37:59 -0600

    feat(manage-order): Implement show all orders method in a talbe on manage-order page

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -7,37 +7,65 @@
         <table class="tbl-full">
             <tr>
                 <th>S.N</th>
-                <th>Full Name</th>
-                <th>Username</th>
+                <th>Food</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th>Order Date</th>
+                <th>Status</th>
+                <th>Customer Name</th>
+                <th>Customer Contact</th>
+                <th>Customer Email</th>
+                <th>Customer Address</th>
                 <th>Actions</th>
             </tr>
-            <tr>
-                <td>1</td>
-                <td>John Doe</td>
-                <td>kleeCollage</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update</a>
-                    <a href="#" class="btn-danger">Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>John Doe</td>
-                <td>kleeCollage</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update</a>
-                    <a href="#" class="btn-danger">Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>John Doe</td>
-                <td>kleeCollage</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update</a>
-                    <a href="#" class="btn-danger">Delete</a>
-                </td>
-            </tr>
+            <?php
+            $sql = "SELECT * FROM tbl_order";
+            $res = mysqli_query($conn, $sql); // Execute query
+            $count = mysqli_num_rows($res); // count rows
+            if ($count > 0)
+            {
+                // Data available
+                while ($row = mysqli_fetch_assoc($res))
+                {
+                    $id = $row['id'];
+                    $food = $row['food'];
+                    $price = $row['price'];
+                    $qty = $row['qty'];
+                    $total = $row['total'];
+                    $order_date = $row['order_date'];
+                    $status = $row['status'];
+                    $customer_name = $row['customer_name'];
+                    $customer_contact = $row['customer_contact'];
+                    $customer_email = $row['customer_email'];
+                    $customer_address = $row['customer_address'];
+            ?>
+                <tr>
+                    <td><?php echo $id ?></td>
+                    <td><?php echo $food ?></td>
+                    <td><?php echo $price ?></td>
+                    <td><?php echo $qty ?></td>
+                    <td><?php echo $total ?></td>
+                    <td><?php echo $order_date ?></td>
+                    <td><?php echo $status ?></td>
+                    <td><?php echo $customer_name ?></td>
+                    <td><?php echo $customer_contact ?></td>
+                    <td><?php echo $customer_email ?></td>
+                    <td><?php echo $customer_address ?></td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update</a>
+                        <a href="#" class="btn-danger">Delete</a>
+                    </td>
+                </tr>
+            <?php
+                }
+            }
+            else
+            {
+                // No data to show
+                echo "<tr><td colspan='12' class='error'>Take a breath. No orders at the moment</td></tr>";
+            }
+            ?>
         </table>
     </div>
 </div>
